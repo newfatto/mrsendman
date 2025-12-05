5c5f7fb64240c3cd9d3bba936d9467c7b45ce29c
Ekaterina Chernyshova <newfatto@gmail.com>
2025-12-05 14:48:42 +0300

    Созданы базовые шаблоны и главная страница для дальнейшего тестирования и развития функциональности

--- mailings/urls.py
+++ mailings/urls.py
@@ -3,12 +3,12 @@ from django.views.decorators.cache import cache_page
 
 from mailings.apps import MailingsConfig
 
-# from catalog.views import
+from mailings import views
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-    #     path("", ListView.as_view(), name="home"),
+        path("", views.index, name="index"),
     #     path("contacts/", TemplateView.as_view(), name="contacts"),
     #     path("product/<int:pk>/", cache_page(60)(DetailView.as_view()), name="product"),
     #     path("product/new/", CreateView.as_view(), name="product_create"),
