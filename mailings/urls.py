d7873181f7136e751c1855cb763cda11f278391f
Ekaterina Chernyshova <newfatto@gmail.com>
2026-01-13 20:31:02 +0300

    Доработана модель Mailing

--- mailings/urls.py
+++ mailings/urls.py
@@ -20,16 +20,18 @@ app_name = MailingsConfig.name
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("/recipients/create/", RecipientCreateView.as_view(), name="recipient_create"),
-    path("/recipients/", RecipientListView.as_view(), name="recipients"),
-    path("/recipients/<int:pk>/confirm_delete/", RecipientDeleteView.as_view(), name="recipient_confirm_delete"),
-    path("/recipients/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
-    path("/recipients/<int:pk>/edit", RecipientUpdateView.as_view(), name="recipient_update"),
-    path("/message/create/", MessageCreateView.as_view(), name="message_create"),
-    path("/messages/", MessageListView.as_view(), name="messages"),
-    path("/message/<int:pk>/confirm_delete/", MessageDeleteView.as_view(), name="message_confirm_delete"),
-    path("/message/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
-    path("/message/<int:pk>/edit", MessageUpdateView.as_view(), name="message_update"),
+
+    path("recipients/create/", RecipientCreateView.as_view(), name="recipient_create"),
+    path("recipients/", RecipientListView.as_view(), name="recipients"),
+    path("recipients/<int:pk>/confirm_delete/", RecipientDeleteView.as_view(), name="recipient_confirm_delete"),
+    path("recipients/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
+    path("recipients/<int:pk>/edit", RecipientUpdateView.as_view(), name="recipient_update"),
+
+    path("message/create/", MessageCreateView.as_view(), name="message_create"),
+    path("messages/", MessageListView.as_view(), name="messages"),
+    path("message/<int:pk>/confirm_delete/", MessageDeleteView.as_view(), name="message_confirm_delete"),
+    path("message/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("message/<int:pk>/edit", MessageUpdateView.as_view(), name="message_update"),
     #     path("contacts/", TemplateView.as_view(), name="contacts"),
     #     path("product/<int:pk>/", cache_page(60)(DetailView.as_view()), name="product"),
     #     path("product/new/", CreateView.as_view(), name="product_create"),
